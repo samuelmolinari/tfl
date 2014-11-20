54f4d41a6a7b7e5905c2dd69edc95e6e9f8cdffb
Samuel Molinari <samuel@molinari.me>
2014-11-20 21:05:20 +0000

    Introduce Placemark

--- lib/tfl.rb
+++ lib/tfl.rb
@@ -1,5 +1,6 @@
 require 'tfl/version'
 require 'tfl/client'
+require 'tfl/placemark'
 require 'tfl/station'
 require 'tfl/api'
 
--- lib/tfl/station.rb
+++ lib/tfl/station.rb
@@ -1,13 +1,4 @@
-require 'tfl/point'
-
 module Tfl
-  class Station
-    attr_accessor :name, :description, :point
-
-    def initialize(options = {})
-      options.each do |key, value|
-        instance_variable_set("@#{key}", value)
-      end
-    end
+  class Station < Placemark
   end
 end
